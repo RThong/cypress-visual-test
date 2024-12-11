5eb401c3e02d0a03d46c8bd1535cb0e4900f70cc
RThong <403216075@qq.com>
2024-12-11 14:16:13 +0900

    update text

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Detail() {
   return (
     <div className="detail-page">
-      <h1>Detail Page11</h1>
+      <h1>Detail Page</h1>
       <a href="/">Back to Home</a>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home-page">
       <h1>Home Page</h1>
-      <a href="/detail">Go to Detail11</a>
+      <a href="/detail">Go to Detail</a>
     </div>
   )
 }
