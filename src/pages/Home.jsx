32395b8fb7f0e65335d24f0f5daff783afe7b16a
RThong <403216075@qq.com>
2024-12-11 14:36:24 +0900

    update text test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="home-page">
       <h1>Home Page</h1>
-      <a href="/detail">Go to Detail</a>
+      <a href="/detail">Go to Detail122</a>
     </div>
   )
 }
